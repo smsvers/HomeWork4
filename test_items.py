673475e212a88a21cb5cab8504bec6d9c4a617ae
smsvers <64587836+smsvers@users.noreply.github.com>
2020-07-07 21:34:09 +0300

    Add files via upload

---
+++ test_items.py
@@ -0,0 +1,8 @@
+from time import sleep
+link ='http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def test_add_basket_button(browser):
+    browser.get(link)
+    sleep(30) #sleep для проверки инициализации браузера
+    add_to_basket = browser.find_element_by_css_selector('.btn-add-to-basket')
+    assert add_to_basket, 'Add button unavailable'
---
+++ test_newuser.py
@@ -0,0 +1,12 @@
+from time import sleep
+from selenium.webdriver.common.by import By
+from random import randint
+link ='http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+
+def test_add_new_user(browser):
+    browser.get(link)
+    browser.find_element(By.ID, "id_registration-email").send_keys(f"asdfasdf2-{randint(10,2000)}@mail.com")
+    browser.find_element(By.ID, "id_registration-password1").send_keys("qwerreeree2342")
+    browser.find_element(By.ID, "id_registration-password2").send_keys("qwerreeree2342")
+    browser.find_element(By.NAME, "registration_submit").click()
+    assert browser.find_element(By.CSS_SELECTOR, ".alertinner").text == "Спасибо за регистрацию!"
